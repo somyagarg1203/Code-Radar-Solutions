0abec7131410db01113cbfb7215dec6888e77ded
somyagarg1203 <somya1693.becse24@chitkara.edu.in>
2025-02-03 12:34:40 +0530

    Solution to Input a Character and Print Its ASCII Value

---
+++ Input a Character and Print Its ASCII Value@@6788d5012db25efec946f93f/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int num1, num2, sum;
+    printf("");
+    scanf("%d", &num1);
+     printf("");
+     scanf("%d", &num2);
+      sum= num1+num2;
+      printf("The sum of %d and %d is %d\n" , num1, num2, sum);
+      return 0;
+}
